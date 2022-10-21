3ba7a457140b773a36cc53c88def64e66c2bb26e
Jesse Robledo <robledo.josej@gmail.com>
2022-10-20 22:48:24 -0600

    updated image links

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,21 +3,21 @@ export const data = [
     name: "Drizzly",
     description:
       "Drizz.ly provides users with a simple interface to give them the weather information they want in a beautiful way.",
-    imageURL: "img",
+    imageURL: "../images/drizzly.png",
     linkURL: "https://drizzly.netlify.app/",
   },
   {
     name: "PodcastApp",
     description:
       "A podcast app that allows users to subscribe to their favorite podcasts.",
-    imageURL: "img",
+    imageURL: "../images/podcast.png",
     linkURL: "https://podcast-app987654.herokuapp.com/",
   },
   {
     name: "Outdoor Voice",
     description:
       "A social app that lets users share multimedia posts on an interactive web map.",
-    imageURL: "img",
+    imageURL: "../images/outdoor.png",
     linkURL:
       "https://trello.com/invite/b/Hr2iWZoZ/a4e768bc26ffaa77d5273966efc852d0/project-management",
   },
