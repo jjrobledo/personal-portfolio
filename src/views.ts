321fb1b3bd7f222687e2e4621b2ec1f0a1aa7779
Jesse Robledo <robledo.josej@gmail.com>
2022-10-20 22:21:20 -0600

    updated render method in ProjectListView class

--- src/views.ts
+++ src/views.ts
@@ -18,14 +18,21 @@ export class View {
       this.projectList = projectList;
     }
   
-    render(): HTMLUListElement {
-      let ul = document.createElement("ul")
+    render(): HTMLDivElement {
+      let div = document.createElement("div")
       this.projectList.getProjects().forEach((project: ProjectTemplate) => {
-        let li = ul.appendChild(document.createElement("li"));
-        li.textContent = project.name;
+        let h2 = div.appendChild(document.createElement("h2"))
+        let p = div.appendChild(document.createElement("p"));
+        let img = div.appendChild(document.createElement("img"))
+        let a = div.appendChild(document.createElement("a"))
+        h2.textContent = project.name
+        p.textContent = project.description;
+        img.src = project.screenshotURL;
+        a.innerHTML = "view the live version"
+        a.href = project.linkURL;
       })
   
-      return ul;
+      return div;
     }
   }
 
