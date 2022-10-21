462df9cf6db3ec5b80babd62154708689424635f
Jesse Robledo <robledo.josej@gmail.com>
2022-10-20 21:38:14 -0600

    changed file extension to .ts

---
+++ src/data/data.ts
@@ -0,0 +1,24 @@
+export const data = [
+  {
+    name: "Drizzly",
+    description:
+      "tDrizz.ly provides users with a simple interface to give them the weather information they want in a beautiful way.his project 1",
+    imageURL: "img",
+    linkURL: "https://drizzly.netlify.app/",
+  },
+  {
+    name: "PodcastApp",
+    description:
+      "A podcast app that allows users to subscribe to their favorite podcasts.",
+    imageURL: "img",
+    linkURL: "https://podcast-app987654.herokuapp.com/",
+  },
+  {
+    name: "Outdoor Voice",
+    description:
+      "A social app that lets users share multimedia posts on an interactive web map.",
+    imageURL: "img",
+    linkURL:
+      "https://trello.com/invite/b/Hr2iWZoZ/a4e768bc26ffaa77d5273966efc852d0/project-management",
+  },
+];
